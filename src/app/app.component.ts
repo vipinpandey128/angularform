99760931665115edca1236d944d4f9eb449b235d
vipin kumar pandey <vipinpandey128@gmail.com>
2022-08-24 21:08:23 +0530

    form control

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, RequiredValidator, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+ 
   title = 'NormalForm-Exercise';
+
+  favoriteColor = new FormControl('',Validators.required);
+  name:string='';
+
+  constructor()
+  {
+
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+  ngOnChange()
+  {
+  }
+
+  ngDoCheck()
+  {
+    console.log(this.favoriteColor);
+  }
 }
